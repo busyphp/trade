61202bca8fd3cda5fbe0f989e416c1ec4d1a7ea4
刘晓彬 <563859671@qq.com>
2022-01-11 11:11:58 +0800

    调整配置名称

--- src/model/TradeConfig.php
+++ src/model/TradeConfig.php
@@ -23,7 +23,7 @@ trait TradeConfig
      */
     public function getTradeConfig($name, $default = null)
     {
-        return App::getInstance()->config->get('trade' . ($name ? ".{$name}" : ''), $default);
+        return App::getInstance()->config->get('busy-trade' . ($name ? ".{$name}" : ''), $default);
     }
     
     
